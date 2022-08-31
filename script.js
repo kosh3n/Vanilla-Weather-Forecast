ba1bf1ad646998260c1249bae3dc66dceaad216e
Nataliia <95924961+kosh3n@users.noreply.github.com>
2022-08-31 16:39:13 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+// data
+let deg = '\u00B0';
+let now = new Date();
+let hours = now.getHours();
+let minutes = now.getMinutes();
+let year = now.getFullYear();
+
+let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+let day = days[now.getDay()];
+
+let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+    'Oct', 'Nov', 'Dec'
+];
+let month = months[now.getMonth()];
+
+let currentData = document.querySelector('#current-data');
+currentData.innerHTML = `${day} ${month}, ${hours}:${minutes}, ${year}`;
+
+// weather api
+let cityName = 'Odesa';
+let apiKey = 'b39a9b967790500d44fb2b61ad166507';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+
+function showTemperature(response) {
+    let temp = document.querySelector('#temp');
+    temp.innerHTML = Math.round(response.data.main.temp) + deg;
+    let descWeather = document.querySelector('#weather-desc');
+    descWeather.innerHTML = response.data.weather[0].description;
+    document.querySelector("#city").innerHTML = response.data.name;
+}
+
+axios.get(`${apiUrl}&appId=${apiKey}`).then(showTemperature);
+
+function searchCity(city) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let city = document.querySelector("#city-input").value;
+    searchCity(city);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+
+// geolocation
+function showPosition(position) {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${
+    position.coords.latitude
+  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showTemperature);
+}
+
+function getCurrentPosition(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+let button = document.querySelector('#current-loc');
+button.addEventListener('click', getCurrentPosition);
